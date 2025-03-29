d55e3370fce4958a1cc284624bdd046d5e65aa23
Shubham K <shubham.kirtinath@yahoo.in>
2025-03-29 17:11:46 +0530

    typ

--- basics/types.c
+++ basics/types.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 char lin[]; //not mentioning size during declaration is somehow legal
-main(){
+/* main(){
     lin[0]='a';
     lin[1]='a';
     lin[2]='a';
     printf("%s",lin);
     printf("%c",lin[3]);
-}
+} */
 //out:aaa
+
+main(){
+    lin[0]='a';
+    lin[1]='b';
+    lin[2]='c';
+    printf("%s",lin); //use string when using array name
+    printf("%c",lin[1]);//character when specific element
+}
+//out:abcb
