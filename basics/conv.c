61974549aba80deae74f416f19ee76b36dfa871e
Shubham K <shubham.kirtinath@yahoo.in>
2025-04-08 23:10:40 +0530

    newfunc

--- basics/conv.c
+++ basics/conv.c
@@ -7,9 +7,22 @@ char s[];
     n = 0;
     for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
         n = 10 * n + s[i] - '0';
-    return(n);
+    return n;
 }
+lower(char s);
 main(){
-   int a=atoi("090"); 
+   int a=atoi("090\n"); 
    printf("%i",a);
+   char c='D';
+   char d='d';
+   printf("%c %c",lower(c),lower(d));
+}
+
+lower(s)
+char s;
+{
+    if(s>='A' && s<='Z'){
+        return s-'A'+'a';
+    }
+    return s;
 }
