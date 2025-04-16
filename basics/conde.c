b1c121fa5ab2dd0582cffd4aab36467d72807c8a
Shubham K <shubham.kirtinath@yahoo.in>
2025-04-16 23:59:15 +0530

    nested if

--- basics/conde.c
+++ basics/conde.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 int main(){
-    int a,b;
+    /* int a,b;
     int c;
     a=10;b=12;
     c=a>b?a:b;
-    printf("%d",c);
+    printf("%d",c); */
+    int n=1;
+    char s[]="101";
+    if (n > 0)
+    for (int i = 0; i < n; i++)
+      if (s[i] > 0) {
+          printf("...");
+          return(i);
+      }
+    else /* WRONG */
+    printf("error - n is zero\n");
 }
