d472808d6e862b7df14bc5dda6c046eaf99a6f97
Shubham K <shubham.kirtinath@yahoo.in>
2025-04-25 23:46:51 +0530

    atoi2

--- basics/conde.c
+++ basics/conde.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int bsearch(int x,int v[],int n);
+int change(char s[],char t[]);
+int atoi(char s[]);
 int main(){
   /* int a,b;
   int c;
@@ -17,9 +19,44 @@ int main(){
     }
     else // n>0
     printf("error - n is zero\n");//else is associated with the closest if */
-    int arx[]={0,1,2,3};
-    printf("%i",bsearch(2,arx,4));
-    
+    /* int arx[]={0,1,2,3};
+    printf("%i",bsearch(2,arx,4)); */
+    /* char source[]="yolo\nto\nthe\n\tmoon";
+    char target[50];
+    change(source,target);
+    printf("%s\n%s",source,target);
+ */
+    printf("%d",atoi("-298"));
+  }
+  int atoi(char s[]){
+    int i,n,sgn;
+    for(i=0;s[i]=='\n'||s[i]=='\t'||s[i]==' ';i++);
+    sgn=1;
+    if( s[i++]=='-')sgn=-1;
+    for(n=0;s[i]>='0'&&s[i]<='9';i++)n=10*n+s[i]-'0';
+    return sgn*n;
+  }
+  int change(char s[],char t[]){
+    int i=0,j=0;
+    while(s[i]!='\0'){
+      char c=s[i];
+      switch(c){
+        case '\n':
+          t[j++]='\\';
+          t[j++]='n';
+          break;
+        case '\t':
+          t[j++]='\\';
+          t[j++]='t';
+          break;
+        default:
+          t[j++]=s[i];
+          break;
+      }
+      i++;
+    }
+    t[j]='\0';
+    return 0;
   }
   int bsearch(int x,int v[],int n){
     int low,high,mid;
